6109a79aedd8d68ec24f2648ca0d90d4b9753065
I.C.AltaValdiSole <tecnico@icaltavaldisole.it>
2024-10-28 12:31:00 +0100

    Update tabella.js

--- assets/js/tabella.js
+++ assets/js/tabella.js
@@ -46,13 +46,8 @@ function caricaLibri() {
                 <td class="${disponibilitaClass}">${disponibilita}</td>
                 <td>
                     ${disponibilita === 'Disponibile' 
-                        ? `<button onclick="prendiInPrestito(${id})"><span>Prendi in prestito</span>
-                        <svg viewBox="-5 -5 110 110" preserveAspectRatio="none" aria-hidden="true">
-                        <path d="M0,0 C0,0 100,0 100,0 C100,0 100,100 100,100 C100,100 0,100 0,100 C0,100 0,0 0,0"/>
-                            </svg></button>`
-                        : `<button onclick="restituisciLibro(${id})"><span>Restituisci</span><svg viewBox="-5 -5 110 110" preserveAspectRatio="none" aria-hidden="true">
-                        <path d="M0,0 C0,0 100,0 100,0 C100,0 100,100 100,100 C100,100 0,100 0,100 C0,100 0,0 0,0"/>
-                            </svg></button>`}
+                        ? `<button onclick="prendiInPrestito(${id})"><span>Prendi in prestito</span></button>`
+                        : `<button onclick="restituisciLibro(${id})"><span>Restituisci</span></button>`}
                 </td>
             `;
             tbody.appendChild(tr);
